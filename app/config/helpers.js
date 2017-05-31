3eb3a340213752f516593314ba127acea0c48e9a
Tom Söderlund <tom@tomsoderlund.com>
2017-05-31 05:48:51 -0400

    No time in list view

--- app/config/helpers.js
+++ app/config/helpers.js
@@ -6,7 +6,8 @@ module.exports = function (app, config) {
 
 	// To string. Months are zero-based
 	app.locals.formatDate = function (dateObj) {
-		return moment(dateObj).format("YYYY-MM-DD HH:mm"); ;
+		//return moment(dateObj).format("YYYY-MM-DD HH:mm"); ;
+		return moment(dateObj).format("YYYY-MM-DD"); ;
 	};
 
 	app.locals.optionalAnchorTag = function (text, href, currentHref) {
